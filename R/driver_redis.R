3824bc1d384a0e5ce0b06eeb6c6474393a7c24c8
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-04-23 19:11:39 +1000

    Move Redis storage from one hash to many keys
    Required for eventual list support

--- R/driver_redis.R
+++ R/driver_redis.R
@@ -24,64 +24,71 @@ driver_redis_api <- function(prefix, con) {
 
   public=list(
     con=NULL,
-    key_hash_value=NULL,
-    key_key_hash=NULL,
+    prefix_data=NULL,
+    prefix_keys=NULL,
 
     initialize=function(prefix, con) {
       self$con <- con
       ## hash -> data mapping
-      self$key_hash_value <- paste0(prefix, ":data")
+      self$prefix_data <- paste0(prefix, ":data")
       ## key -> hash mapping
-      self$key_key_hash <- paste0(prefix, ":hash")
+      self$prefix_keys <- paste0(prefix, ":hash")
     },
 
     exists_hash=function(hash) {
-      self$con$HEXISTS(self$key_hash_value, hash) == 1L
+      self$con$EXISTS(self$name_data(hash)) == 1L
     },
     exists_key=function(key) {
-      self$con$HEXISTS(self$key_key_hash, key) == 1L
+      self$con$EXISTS(self$name_key(key)) == 1L
     },
 
     ## Write some data into its hash value
     set_hash_value=function(hash, value) {
-      ## This is not ideal because we'll have hashed the object twice
-      ## to get here, but I don't see how else to get around it...
-      str <- object_to_string(value)
-      self$con$HSET(self$key_hash_value, hash, str)
+      self$con$SET(self$name_data(hash), object_to_string(value))
     },
     ## Associate a key with some data
     set_key_hash=function(key, hash) {
-      self$con$HSET(self$key_key_hash, key, hash)
+      self$con$SET(self$name_key(key), hash)
     },
 
     ## Get value, given hash
     get_value=function(hash) {
-      if (self$exists_hash(hash)) {
-        string_to_object(self$con$HGET(self$key_hash_value, hash))
+      name <- self$name_data(hash)
+      if (self$con$EXISTS(name)) {
+        string_to_object(self$con$GET(name))
       } else {
         stop(HashError(hash))
       }
     },
     ## Get hash, given key
     get_hash=function(key) {
-      if (self$exists_key(key)) {
-        self$con$HGET(self$key_key_hash, key)
+      name <- self$name_key(key)
+      if (self$con$EXISTS(name)) {
+        self$con$GET(name)
       } else {
         stop(KeyError(key))
       }
     },
 
-    list_hashes=function() {
-      as.character(self$con$HKEYS(self$key_hash_value))
-    },
-    list_keys=function() {
-      as.character(self$con$HKEYS(self$key_key_hash))
-    },
-
     del_hash=function(hash) {
-      self$con$HDEL(self$key_hash_value, hash) == 1L
+      self$con$DEL(self$name_data(hash)) == 1L
     },
     del_key=function(key) {
-      self$con$HDEL(self$key_key_hash, key) == 1L
+      self$con$DEL(self$name_key(key)) == 1L
+    },
+
+    ## Potentially expensive!
+    list_hashes=function() {
+      keys_minus_prefix(self$con, self$name_data(""))
+    },
+    list_keys=function() {
+      keys_minus_prefix(self$con, self$name_key(""))
+    },
+
+    name_data=function(hash) {
+      sprintf("%s:%s", self$prefix_data, hash)
+    },
+    name_key=function(key) {
+      sprintf("%s:%s", self$prefix_keys, key)
     }
   ))
--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,11 @@ rm0 <- function(list, envir) {
   rm(list=list[del], envir=envir)
   del
 }
+
+str_drop_start <- function(x, sub) {
+  substr(x, nchar(sub) + 1L, nchar(x))
+}
+
+keys_minus_prefix <- function(con, prefix) {
+  str_drop_start(as.character(con$KEYS(paste0(prefix, "*"))), prefix)
+}
