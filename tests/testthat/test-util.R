8c0172237847a675efe208e3d9acde7422247005
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-11-11 08:21:31 +0000

    Assertion tests

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -17,3 +17,19 @@ test_that("write_bin", {
 
   expect_identical(readBin(path, raw(), 1000), bytes)
 })
+
+test_that("assertions", {
+  expect_error(assert_scalar(NULL), "must be a scalar")
+  expect_error(assert_scalar(1:2), "must be a scalar")
+
+  expect_error(assert_length(1, 2), "must have 2 elements")
+
+  expect_error(assert_function("sin"), "must be a function")
+
+  expect_error(assert_environment("foo"), "must be an environment")
+
+  expect_error(assert_list(1:5), "must be a list")
+  expect_error(assert_logical(1:5), "must be logical")
+  expect_error(assert_character(1:5), "must be character")
+  expect_error(assert_raw(1:5), "must be raw")
+})
