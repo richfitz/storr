04d563f20355c7b1d3c61ff2c838469ea8ab4294
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-11-30 08:41:55 +0000

    Include some test helpers

---
+++ tests/testthat/helper-storr.R
@@ -0,0 +1,14 @@
+rand_str <- function(n = 16L, hex = TRUE) {
+  if (hex) {
+    paste(sample(as.raw(0:255), n, replace = TRUE), collapse = "")
+  } else {
+    rawToChar(sample(as.raw(32:126), n, replace = TRUE))
+  }
+}
+
+skip_long_test <- function() {
+  if (identical(Sys.getenv("STORR_RUN_LONG_TESTS"), "true")) {
+    return(invisible(TRUE))
+  }
+  skip("Skipping long running test")
+}
