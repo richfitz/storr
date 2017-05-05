cbf1808235b6b9aa661225ea929842df29e26bff
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-05-05 12:07:42 +0100

    Nicer printing of the spec tests
    Hopefully this will survive one or two versions of testthat changes

--- tests/testthat/test-spec.R
+++ tests/testthat/test-spec.R
@@ -2,10 +2,13 @@ context("spec")
 
 test_that("reporter handling", {
   create <- function(dr = NULL, ...) driver_environment(dr$envir, ...)
-  expect_message(r <- testthat::with_reporter(
-    testthat::StopReporter,
-    ans <- test_driver(create)),
-    "PASS")
-  expect_is(r, "StopReporter")
+  res <- testthat::evaluate_promise(
+    testthat::with_reporter(
+      testthat::StopReporter,
+      ans <- test_driver(create)))
+
+  expect_is(res$result, "StopReporter")
+  expect_match(res$messages, "PASS")
+  expect_match(res$output, "DONE")
   expect_is(ans, "data.frame")
 })
