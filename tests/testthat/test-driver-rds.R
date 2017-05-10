richfitz/storr

commit f954bf0fcab2370a003f96656ad13d3d67bd7217

Add support for skipping padding in encode64
The RFC is pretty explicit that this should be done

> Implementations MUST include appropriate pad characters at then
> end of encoded data unless the specification referring to this document
> explicitly states otherwise
>
> The base64 and base32 alphabets use padding...

(section 3.2), but _empirically_ it seems to work OK without it and this
is tested (and of course used in the base64url package, which I don't
think I knew about at the time I wrote this).

For #43

R/base64.R
@@ -6,6 +6,8 @@
 ##' @param x A string or vector of strings to encode/decode
 ##' @param char62 Character to use for the 62nd index
 ##' @param char63 Character to use for the 63rd index
+##' @param pad Logical, indicating if strings should be padded with
+##'   \code{=} characters (as RFC 4648 requires)
 ##' @export
 ##' @examples
 ##' x <- encode64("hello")
@@ -15,7 +17,7 @@
 ##' # Encoding things into filename-safe strings is the reason for
 ##' # this function:
 ##' encode64("unlikely/to be @ valid filename")
-encode64 <- function(x, char62 = "-", char63 = "_") {
+encode64 <- function(x, char62 = "-", char63 = "_", pad = TRUE) {
   if (length(x) != 1L) {
     return(vcapply(x, encode64, char62, char63, USE.NAMES = FALSE))
   }
@@ -38,7 +40,7 @@ encode64 <- function(x, char62 = "-", char63 = "_") {
   z <- tr[bitwAnd(y, 63L) + 1L]
   if (n_pad > 0) {
     len <- length(z)
-    z[(len - n_pad + 1):len] <- "="
+    z[(len - n_pad + 1):len] <- if (pad) "=" else ""
   }
   paste0(z, collapse = "")
 }

tests/testthat/test-base64.R
@@ -38,3 +38,25 @@ test_that("vector", {
   expect_equal(x, vcapply(letters, encode64, USE.NAMES = FALSE))
   expect_equal(decode64(x), letters)
 })
+
+test_that("padding", {
+  ## This duplicates much of the code above.
+  rand_str_len <- function(n) {
+    pos <- as.raw(32:126)
+    rawToChar(sample(pos, n, replace = TRUE))
+  }
+  for (len in 1:20) {
+    for (r in 1:20) {
+      s <- rand_str_len(len)
+      t1 <- encode64(s, pad = TRUE)
+      t2 <- encode64(s, pad = FALSE)
+      expect_identical(decode64(t1), s)
+      expect_identical(decode64(t2), s)
+
+      n_pad <- 2 - (len - 1) %% 3
+      expect_equal(substr(t1, nchar(t1) - n_pad + 1, nchar(t1) + 1),
+                   paste(rep("=", n_pad), collapse = ""))
+      expect_equal(nchar(t1), nchar(t2) + n_pad)
+    }
+  }
+})

man/encode64.Rd
@@ -5,7 +5,7 @@
 \alias{encode64}
 \title{Base64 encoding and decoding}
 \usage{
-encode64(x, char62 = "-", char63 = "_")
+encode64(x, char62 = "-", char63 = "_", pad = TRUE)
 
 decode64(x, char62 = "-", char63 = "_")
 }
@@ -15,6 +15,9 @@ decode64(x, char62 = "-", char63 = "_")
 \item{char62}{Character to use for the 62nd index}
 
 \item{char63}{Character to use for the 63rd index}
+
+\item{pad}{Logical, indicating if strings should be padded with
+\code{=} characters (as RFC 4648 requires)}
 }
 \description{
 Base64 encoding.  By default uses the RFC 4648 dialect (file/url

commit 14cb20d70222d8f7c959ba530e97837cfc637afe

Fix a bug introduced in f954bf0
Where >1 name is encoded, the pad argument is not passed along, so
names are padded!

R/base64.R
@@ -19,7 +19,7 @@
 ##' encode64("unlikely/to be @ valid filename")
 encode64 <- function(x, char62 = "-", char63 = "_", pad = TRUE) {
   if (length(x) != 1L) {
-    return(vcapply(x, encode64, char62, char63, USE.NAMES = FALSE))
+    return(vcapply(x, encode64, char62, char63, pad, USE.NAMES = FALSE))
   }
   tr <- c(LETTERS, letters, 0:9, char62, char63)
   x <- as.integer(charToRaw(x))

tests/testthat/test-base64.R
@@ -74,3 +74,10 @@ test_that("padding", {
   expect_identical(length(err3), 0L)
   expect_identical(length(err4), 0L)
 })
+
+test_that("vector encode", {
+  v <- c("x", "xx", "xxx")
+  cmp <- vcapply(v, encode64, pad = TRUE, USE.NAMES = FALSE)
+  expect_equal(encode64(v, pad = TRUE), cmp)
+  expect_equal(encode64(v, pad = FALSE), sub("=+$", "", cmp))
+}

tests/testthat/test-driver-rds.R
@@ -217,3 +217,14 @@ test_that("copy -lr and consistency", {
   expect_equal(st2$list_hashes(), h1)
   expect_equal(sort(st1$list_hashes()), sort(c(h1, h2)))
 })
+
+## Prevent a regression
+test_that("vectorised exists", {
+  st <- storr_rds(tempfile(), mangle_key = TRUE)
+  on.exit(st$destroy())
+
+  expect_equal(st$exists(c("x", "xx")), c(FALSE, FALSE))
+  st$set("x", 1)
+  st$set("xx", 2)
+  expect_equal(st$exists(c("x", "xx")), c(TRUE, TRUE))
+})