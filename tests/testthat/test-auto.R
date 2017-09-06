f00d047b15c987d1c65d4fec7cd0d7b7ba0c3954
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-09-06 07:37:57 +0100

    Avoid running redis tests in inappropriate settings

--- tests/testthat/test-auto.R
+++ tests/testthat/test-auto.R
@@ -32,6 +32,7 @@ pg_tester <- function(ctor) {
 }
 
 test_that("dbi (postgres via RPostgres)", {
+  skip_on_cran()
   if (requireNamespace("RPostgres", quietly = TRUE)) {
     if (has_postgres(RPostgres::Postgres)) {
       storr::test_driver(pg_tester(RPostgres::Postgres))
@@ -40,6 +41,7 @@ test_that("dbi (postgres via RPostgres)", {
 })
 
 test_that("dbi (postgres via RPostgreSQL)", {
+  skip_on_cran()
   if (requireNamespace("RPostgreSQL", quietly = TRUE)) {
     oo <- options(warnPartialMatchArgs = FALSE)
     if (!is.null(oo$warnPartialMatchArgs)) {
@@ -51,16 +53,20 @@ test_that("dbi (postgres via RPostgreSQL)", {
   }
 })
 
-## These are not required on CRAN testing, but only for my own
-## edification.
-if ("redux" %in% .packages(TRUE)) {
-  con <- redux::hiredis()
-  storr::test_driver(function(dr = NULL, ...)
-    driver_redis_api(dr$prefix %||% rand_str(), con, ...))
-}
-
-if ("rrlite" %in% .packages(TRUE)) {
-  con <- rrlite::hirlite()
-  storr::test_driver(function(dr = NULL, ...)
-    driver_redis_api(dr$prefix %||% rand_str(), con, ...))
-}
+test_that("redis_api", {
+  skip_on_cran()
+  if (!identical(Sys.getenv("ISOLATED_REDIS"), "true")) {
+    skip("No isolated Redis server to work with")
+  }
+  has_redis <- tryCatch(redux::redis_available(), error = function(e) FALSE)
+  if ("redux" %in% .packages(TRUE)) {
+    con <- redux::hiredis()
+    storr::test_driver(function(dr = NULL, ...)
+      driver_redis_api(dr$prefix %||% rand_str(), con, ...))
+  }
+  ## if ("rrlite" %in% .packages(TRUE)) {
+  ##   con <- rrlite::hirlite()
+  ##   storr::test_driver(function(dr = NULL, ...)
+  ##     driver_redis_api(dr$prefix %||% rand_str(), con, ...))
+  ## }
+})
