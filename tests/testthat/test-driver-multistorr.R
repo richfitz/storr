4ebcf7b8c78c1ca02da903d102137644c4f44226
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-05-25 17:10:11 +0100

    Fix tests

--- tests/testthat/test-driver-multistorr.R
+++ tests/testthat/test-driver-multistorr.R
@@ -8,7 +8,7 @@ test_that("creation", {
   st$set("a", mtcars)
 
   cmp <- storr_rds(path)
-  expect_equal(cmp$list_hashes(), st$list_hashes()))
+  expect_equal(cmp$list_hashes(), st$list_hashes())
   expect_equal(cmp$list(), character())
   expect_equal(st$list(), "a")
 
@@ -18,6 +18,7 @@ test_that("creation", {
 
 
 test_that("storr driver detection", {
+  e <- driver_environment()
   expect_silent(assert_probably_storr_driver(e))
   expect_error(assert_probably_storr_driver(e$type),
                "does not look like a storr driver")
