580a1230c217c6e45fd1c7479ce12d8b575bdd6e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-05-30 08:23:27 +0100

    Test additional branch in remote storr
    Revealed by improvements to covr

--- tests/testthat/test-driver-remote.R
+++ tests/testthat/test-driver-remote.R
@@ -36,3 +36,26 @@ test_that("local cache", {
   expect_equal(dr$rds$list_keys("objects"), character())
   expect_equal(dr$list_keys("objects"), "a")
 })
+
+
+test_that("list keys works in both manglings", {
+  p1 <- tempfile()
+  d1 <- driver_remote(fake_file_ops(tempfile()), mangle_key = FALSE)
+  s1 <- storr(d1)
+
+  p2 <- tempfile()
+  d2 <- driver_remote(fake_file_ops(tempfile()), mangle_key = TRUE)
+  s2 <- storr(d2)
+
+  on.exit({
+    s1$destroy()
+    s2$destroy()
+  })
+
+  k <- "silly key name"
+  s1$set(k, 1)
+  s2$set(k, 1)
+
+  expect_equal(s1$list(), k)
+  expect_equal(s2$list(), k)
+})
