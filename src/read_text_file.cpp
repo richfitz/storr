ee0342ed04ac4f134700ce26338e8b4f4c1ff68a
wlandau-lilly <will.landau@lilly.com>
2019-01-08 17:05:20 -0500

    Fix fread() issue
    https://wiki.sei.cmu.edu/confluence/pages/viewpage.action?pageId=87152233

--- src/read_text_file.cpp
+++ src/read_text_file.cpp
@@ -6,10 +6,11 @@
  */
 extern "C" SEXP read_text_file(SEXP path, SEXP nchar) {
   size_t size = asInteger(nchar) * sizeof(char);
-  char *buf = (char*) malloc(size);
+  char *buf = (char*) calloc(1, size);
   FILE *fp;
   fp = fopen(CHAR(asChar(path)), "rb");
   fread(buf, size, 1, fp);
+  buf[size] = '\0'; // Very important: https://wiki.sei.cmu.edu/confluence/pages/viewpage.action?pageId=87152233
   fclose(fp);
   SEXP out = PROTECT(allocVector(STRSXP, 1));
   SET_STRING_ELT(out, 0, mkChar(buf));
